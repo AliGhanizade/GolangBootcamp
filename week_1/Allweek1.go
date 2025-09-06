0b6a00d1b9d4ff4322f3b5c09ca9c623fd570bac
AliGhanizade <thealighanizade@gmail.com>
2025-09-06 20:27:28 +0330

    add select result

--- week_1/Allweek1.go
+++ week_1/Allweek1.go
@@ -1,6 +1,43 @@
 package week_1
 
-import common "GolangBootcamp/common"
+import (
+	common "GolangBootcamp/common"
+	"fmt"
+)
+
+func SelectResult() {
+	Numbers := randomNumberForSlice()
+	common.PrintByTime(Numbers)
+	var practice int
+	fmt.Print("Write Number of practice 1-Average 2-Sum 3-Average&Sum 4-Maximum 5-Minimum  0-All :")
+	fmt.Scan(&practice)
+
+	switch practice {
+
+	case 1:
+		av := averageSlice(Numbers)
+		common.PrintByTime(av)
+
+	case 2:
+		sum := sumSlice(Numbers)
+		common.PrintByTime(sum)
+
+	case 3:
+		sum, average := sumAndAverageSlice(Numbers)
+		common.PrintByTime(sum, average)
+
+	case 4:
+		max := maximumNumberInSlice(Numbers)
+		common.PrintByTime(max)
+
+	case 5:
+		min := minimumNumberInSlice(Numbers)
+		common.PrintByTime(min)
+
+	case 0:
+		AllWeek1()
+	}
+}
 
 func AllWeek1() {
 	var Numbers []int
