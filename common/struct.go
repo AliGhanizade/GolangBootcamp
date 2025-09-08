986c507651f8936f1294028d17aa9288205ffb47
AliGhanizade <thealighanizade@gmail.com>
2025-09-08 18:29:59 +0330

    add json name for structs

--- common/struct.go
+++ common/struct.go
@@ -1,15 +1,15 @@
 package common
 
 type Person struct {
-	ID          int
-	Name        string
-	Age         int
-	PhoneNumber string
-	Email       string
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Age         int    `json:"age"`
+	PhoneNumber string `json:"phone_number"`
+	Email       string `jsom:"email,omitempty"`
 }
 
 type Username struct {
-	DateTime    string
-	Username    string
-	PhoneNumber string
+	DateTime    string `json:"date_time"`
+	Username    string `json:"username"`
+	PhoneNumber string `json:"phone_number"`
 }
