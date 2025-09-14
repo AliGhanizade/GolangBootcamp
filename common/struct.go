5f2ed713eb588d04e46f1546117e2bf468e19177
AliGhanizade <thealighanizade@gmail.com>
2025-09-14 14:49:39 +0330

    add logreader struct for save

--- common/struct.go
+++ common/struct.go
@@ -13,3 +13,15 @@ type Username struct {
 	Username    string `json:"username"`
 	PhoneNumber string `json:"phone_number"`
 }
+
+type LogReader struct {
+	GET int `json:"GET"`
+	POST int `json:"POST"`
+	DELETE int `json:"DELETE"`
+	PUT int `json:"UPDATE"`
+	Error int `json:"error"`
+	Alert int `json:"alert"`
+	Warn int `json:"warn"`
+	Crit int `json:"crit"`
+	TotalRequests int `json:"total_requests"`
+}	
