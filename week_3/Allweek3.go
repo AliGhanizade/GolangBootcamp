24841fa52a7797bd6c9a66392e95d2116f022ed2
AliGhanizade <thealighanizade@gmail.com>
2025-09-29 14:15:26 +0330

    add worker pool to select and show result

--- week_3/Allweek3.go
+++ week_3/Allweek3.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func SelectResult() {
 	var practice int
-	fmt.Print("Write Number of practice 1-Print 2-useChannel 0-All:")
+	fmt.Print("Write Number of practice 1-Print 2-useChannel 3-WorkerPool 0-All:")
 	fmt.Scan(&practice)
 	switch practice {
 	case 1:
 		runPrinterGoroutine()
 	case 2:
 		useChannel()
+	case 3:
+		StartWorkerPool()
+
 	}
 }
 
